f3c8e0a1145afb6cf0bc49d8c3a584dc2e75cb25
Ashish <40253700+ashishsj9@users.noreply.github.com>
2020-01-22 11:58:55 -0500

    update

--- validateDOB.py
+++ validateDOB.py
@@ -1,18 +1,20 @@
-import datetime
+
+from datetime import datetime
 
 
-def validatedob(name, dob):
-    dob_obj = datetime.datetime.strptime(dob, "%m/%d/%y")
-    birth_year = dob_obj.year
-    current_year = datetime.date.today().year
+def validateDOB(name, dob):
+    dob_obj = datetime.strptime(dob, "%d/%m/%Y")
+    birthYear = dob_obj.year
+    currentDate = datetime.now()
+    currentYear = currentDate.year
 
-    if current_year < birth_year:
-        age = birth_year - current_year
+    if currentYear < birthYear:
+        age = birthYear - currentYear
     else:
-        age = current_year - birth_year - 1
+        age = currentYear - birthYear - 1
     print(age)
     print("Hello " + name + ", you are " + str(age) + " old")
 
 
 if __name__ == '__main__':
-    validatedob("ashish", "05/10/1993")
+    validateDOB("ashish", "05/10/1993")
