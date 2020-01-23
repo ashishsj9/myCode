bf2fa9b2d304d28315d2c0523dc95154994aa653
Ashish <40253700+ashishsj9@users.noreply.github.com>
2020-01-23 07:04:26 -0500

    Updated

---
+++ Height-Weight_GenderPrediction.py
@@ -0,0 +1,6 @@
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
--- readCSVPandas.py
+++ readCSVPandas.py
@@ -1,3 +1,8 @@
 import pandas
+import matplotlib.pyplot as plt
+
 df = pandas.read_csv('/Users/ashishjhade/Documents/myCode/data/weight-height.csv')
-print(df)
+#print(df)
+
+df.plot.scatter(x='Height', y='Weight', c='black')
+plt.show()
